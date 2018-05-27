ac97e12c4ae9888d9ad2b5e3a1cbaa70dd88e709
lzm <763311958@qq.com>
2018-05-27 21:45:34 +0800

    2018-5-27

---
+++ xjzx/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+def create_app(config):
+    app = Flask(__name__)
+
+    app.config.from_object(config)
+
+    # 注册蓝图
+    from views_admin import admin_blueprint
+    app.register_blueprint(admin_blueprint)
+
+    from views_user import user_blueprint
+    app.register_blueprint(admin_blueprint)
+
+    from views_news import news_blueprint
+    app.register_blueprint(admin_blueprint)
+
+
+
+
+    return app
---
+++ xjzx/views_news.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+news_blueprint = Blueprint('user',__name__,url_prefix='/user')
+
